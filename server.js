cc00d407a35da09190f96a0a07f28c032b1fa019
rowieej <rowena.jaico@gmail.com>
2016-09-09 18:51:10 -0400

    changed database configuration with mongoose

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ app.use(express.static('public'));
 
 
 // Database configuration with mongoose
-mongoose.connect('mongodb://localhost/week18day3mongoose');
+//mongoose.connect('mongodb://localhost/week18day3mongoose');
+mongoose.connect('mongodb://localhost/scraper');
 var db = mongoose.connection;
 
 // show any mongoose errors
@@ -52,11 +53,11 @@ app.get('/', function(req, res) {
 // A GET request to scrape the echojs website.
 app.get('/scrape', function(req, res) {
   // first, we grab the body of the html with request
-  request('http://www.echojs.com/', function(error, response, html) {
+  request('http://www.huffingtonpost.com/', function(error, response, html) {
     // then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(html);
     // now, we grab every h2 within an article tag, and do the following:
-    $('article h2').each(function(i, element) {
+    $('h2').each(function(i, element) {
 
         // save an empty result object
         var result = {};
